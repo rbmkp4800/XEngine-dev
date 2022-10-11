1e9f6a34f8f18d6dae567ad6d72d4dab24244b5f
Rostyslav Kotoshchuk <rbmkp4800@gmail.com>
2022-10-11 13:29:33 +0300

    XLib.CRC: Used CRC32/CRC64 algo clarifications

--- XLib/Source/XLib.CRC.h
+++ XLib/Source/XLib.CRC.h
@@ -4,6 +4,14 @@
 
 // TODO: implement standart algorithms
 
+//	| Algorithm       | Polynomial         | XorIn              | XorOut             | RefIn | RefOut | Check(123456789)   |
+//	+-----------------+--------------------+--------------------+--------------------+-------+--------+--------------------+
+//	| CRC-32/zlib     | 0x04C11DB7         | 0xFFFFFFFF         | 0xFFFFFFFF         |   +   |   +    | 0xCBF43926         |
+//	| CRC-32/POSIX    | 0x04C11DB7         | 0x00000000         | 0xFFFFFFFF         |   -   |   -    | 0x765E7680         |
+//	| CRC-64/ECMA182  | 0x42F0E1EBA9EA3693 | 0x0000000000000000 | 0x0000000000000000 |   -   |   -    | 0x6C40DF5F0B497347 |
+//	| CRC-64/XZ       | 0x42F0E1EBA9EA3693 | 0xFFFFFFFFFFFFFFFF | 0xFFFFFFFFFFFFFFFF |   +   |   +    | 0x995DC9BBDF1939FA |
+//	| CRC-64/ISO      | 0x000000000000001B | 0xFFFFFFFFFFFFFFFF | 0xFFFFFFFFFFFFFFFF |   +   |   +    | 0xB90956C775A41001 |
+
 namespace XLib
 {
 	class CRC8
@@ -42,7 +50,7 @@ namespace XLib
 		static inline uint16 Compute(const Type& data) { return Compute(&data, sizeof(data)); }
 	};
 
-	class CRC32
+	class CRC32 // CRC-32/zlib
 	{
 	private:
 		uint32 value = 0;
@@ -60,7 +68,7 @@ namespace XLib
 		static inline uint32 Compute(const Type& data) { return Compute(&data, sizeof(data)); }
 	};
 
-	class CRC64
+	class CRC64 // CRC-64/XZ
 	{
 	private:
 		uint64 value = 0;
