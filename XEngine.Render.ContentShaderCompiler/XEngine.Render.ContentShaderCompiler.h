68fddd1d685f2085fbfe0681318ee773fb18a060
Rostyslav Kotoshchuk <rbmkp4800@gmail.com>
2024-09-24 12:09:32 +0200

    WIP

---
+++ XEngine.Render.ContentShaderCompiler/XEngine.Render.ContentShaderCompiler.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace XEngine::Render::ContentShaderCompiler
+{
+	void CompileSurfaceShader();
+
+	void CompileVolumeShader();
+
+	void CompileDecalShader();
+
+	void CompileLightProfileShader();
+
+	void CompilePostProcessShader();
+}
